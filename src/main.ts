6d03f98b338a8dbf8c7cddb6361356ea6ad676ba
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-05-27 09:40:26 +0200

    added cors 'https://ievgenbilion.github.io',

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://ievgenbilion.developerakademie.net'], // Specify the exact origin of your frontend
+    origin: ['http://localhost:4200', 'https://ievgenbilion.developerakademie.net', 'https://ievgenbilion.github.io'], // Specify the exact origin of your frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Allow cookies, authorization headers, etc. (if needed)
   });
